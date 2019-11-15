3241259982ab578d994492de9787f6115402566d
lichihang <lichihang@banggood.com>
2019-11-15 15:45:17 +0800

    初始化自定义的vue-cli脚手架

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# customWebpackVueCli
-自定义的vue-cli脚手架
+# vue-webpack-boilerplate(基于vue-cli定制满足自己配置的脚手架，支持自定义配置，较为灵活)
+
+
+## Usage
+
+This is a project template for [vue-cli](https://github.com/vuejs/vue-cli). **It is recommended to use npm 3+ for a more efficient dependency tree.**
+
+## Document
+里面有接口的封装，生产模式的封装，store的集成等
+执行步骤如下
+
+##
+``` bash
+$ npm install -g vue-cli
+$ vue init qyc437970808/webpack#qyc437970808 my-project
+$ cd my-project
+$ npm install
+$ npm run dev
+```
+##
+
---
+++ template/src/main.js
@@ -0,0 +1,32 @@
+{{#if_eq build "standalone"}}
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+{{/if_eq}}
+import Vue from 'vue'
+import App from './App'
+{{#router}}
+import router from './router'
+{{/router}}
+
+import store from './store/index';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+//看看配置会不会生效
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  {{#router}}
+  router,
+  {{/router}}
+  store,
+  {{#if_eq build "runtime"}}
+  render: h => h(App)
+  {{/if_eq}}
+  {{#if_eq build "standalone"}}
+  components: { App },
+  template: '<App/>'
+  {{/if_eq}}
+})
